ac5cfd8bd8814091e5dd2b84cbf223038371710c
Paul Madut <paulmadut@mths.ca>
2023-10-06 20:41:34 -0400

    Added user schemas

---
+++ Backend/Models/Consumers.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+function validateUser(hash) {
+  bcrypt.compare(hash, this.password, function (err, res) {
+    if (err) {
+      console.log(err);
+    }
+    return res;
+  });
+}
+
+const userModel = mongoose.model("users", UserSchema);
+module.exports = userModel;
