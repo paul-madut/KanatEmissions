717244da8006bb91fd844b30cb11d0def6544be8
Paul Madut <paulmadut@mths.ca>
2023-10-08 01:53:00 -0400

    Registration

--- Frontend/kanatemissions/src/Components/Login/Login.jsx
+++ Frontend/kanatemissions/src/Components/Login/Login.jsx
@@ -1,4 +1,15 @@
 import React, { useState } from 'react'
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Checkbox,
+    Button,
+} from "@material-tailwind/react";
+
 function Login() {
 
     const [userName, setUserName] = useState('');
@@ -22,7 +33,43 @@ function Login() {
     }
     return (
         <>
-            <div className='background flex justify-center h-screen w-screen bg-cover bg-[url("https://images.unsplash.com/photo-1488554378835-f7acf46e6c98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80")]'></div>
+            <div className='background flex items-center justify-center h-screen w-screen bg-cover bg-[url("https://images.unsplash.com/photo-1488554378835-f7acf46e6c98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80")]'>
+                <Card className="w-96 h-5/6 p-6 flex  justify-center">
+                    <CardHeader
+                        variant="gradient"
+                        color="gray"
+                        className="mb-4 grid h-28 place-items-center"
+                    >
+                        <Typography variant="h3" color="white">
+                            Sign In
+                        </Typography>
+                    </CardHeader>
+                    <CardBody className="flex flex-col gap-4">
+                        <Input label="Email" size="lg" />
+                        <Input label="Password" size="lg" />
+                        <div className="-ml-2.5">
+                            <Checkbox label="Remember Me" />
+                        </div>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                        <Button variant="gradient" fullWidth>
+                            Sign In
+                        </Button>
+                        <Typography variant="small" className="mt-6 flex justify-center">
+                            Don&apos;t have an account?
+                            <Typography
+                                as="a"
+                                href="#signup"
+                                variant="small"
+                                color="blue-gray"
+                                className="ml-1 font-bold"
+                            >
+                                Sign up
+                            </Typography>
+                        </Typography>
+                    </CardFooter>
+                </Card>
+            </div>
         </>
     )
 }
